6e128fb84a49917c13a252e2102d8bccab5e30b0
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2019-02-10 22:49:19 +0000

    broken mess in terms of auth

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,7 +49,9 @@ RSpec.configure do |config|
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
   # add support stuff
-  config.include RequestSpecHelper, type: :request
+  #config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
